c1cc5ae5b7f000225b5a139f3de4794d8887c26c
ErwinPrograms <erwinprograms@gmail.com>
2026-08-21 09:49:37 -0700

    Update so enemy plays death sound only once

--- scenes/entities/characters/bug_enemy.gd
+++ scenes/entities/characters/bug_enemy.gd
@@ -2,6 +2,7 @@ class_name BugEnemy
 extends CharacterBody2D
 
 var health: int = 3
+var dead: bool = false
 
 func take_damage(damage: int) -> void:
 	health -= damage
@@ -16,7 +17,10 @@ func die() -> void:
 	# disconnect from signals, remove hitbox
 	# automatically remove after some time
 	
+	if dead:
+		return
 	#placeholder queue free
+	dead = true
 	$FmodEventEmitter2D.play_one_shot()
 	EventBus.enemy_died.emit()
 	await get_tree().create_timer(1).timeout
