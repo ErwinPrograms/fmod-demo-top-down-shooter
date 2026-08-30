0b8c8e2c0073d9b66ed3eb74bb555b6b15f99d02
ErwinPrograms <erwinprograms@gmail.com>
2026-08-30 13:42:05 -0700

    Add ability to hide ui by pressing escape

--- scenes/ui/instant_die_button.gd
+++ scenes/ui/instant_die_button.gd
@@ -1,5 +1,8 @@
 extends Button
 
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		visible = not visible
 
 func _on_pressed() -> void:
 	PlayerCharacter.Instance.die()
--- scenes/ui/music_track_slider_menu.gd
+++ scenes/ui/music_track_slider_menu.gd
@@ -3,6 +3,10 @@ extends PanelContainer
 const AUTOMATION_ONE_NAME: String = "Automation"
 const AUTOMATION_TWO_NAME: String = "Health"
 
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		visible = not visible
+
 func _on_automation_h_slider_value_changed(value: float) -> void:
 	FmodSoundtrackEmitter2D.instance.set_parameter(AUTOMATION_ONE_NAME, value)
 
