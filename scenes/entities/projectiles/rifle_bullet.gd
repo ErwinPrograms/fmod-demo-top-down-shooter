9866710f76b7f741576b6a659f17dbdb5d84e1f8
ErwinPrograms <erwinprograms@gmail.com>
2026-08-30 10:28:43 -0700

    Add bullet trail lines with a small visual bug

--- scenes/entities/projectiles/rifle_bullet.gd
+++ scenes/entities/projectiles/rifle_bullet.gd
@@ -4,18 +4,29 @@ extends Area2D
 @export var direction: Vector2
 @export var speed: float = 200.0
 @export var damage: int = 2
+@export var trail_length: float = 100
 #damage
 #owner
 
 #range
 #starting at
 # lifetime
+@onready var trail_line: Line2D = $TrailLine
 
 func _process(delta: float) -> void:
 	if !direction or !direction.is_normalized():
 		print("Projectile invalid direction")
+	adjust_trail_line(delta)
 	
 	position += direction * speed * delta
+
+func adjust_trail_line(delta: float) -> void:
+	var tail_pos: Vector2 = trail_line.points[0]
+	var head_pos: Vector2 = trail_line.points[1] # should always equal 0,0
+	if (head_pos - tail_pos).length() < trail_length:
+		tail_pos -= direction * speed * delta
+		tail_pos = tail_pos.normalized() * min(tail_pos.length(), trail_length)
+	trail_line.points = PackedVector2Array([tail_pos, head_pos])
 
 func _on_lifespan_timer_timeout() -> void:
 	queue_free()
